e496c351cca1a9cf0969fb4e5ab552362c95c1d1
Jain <AB54364@dnbnor.net>
2020-08-06 15:06:33 +0200

    indent  fix

--- s3.tf
+++ s3.tf
@@ -3,6 +3,9 @@ resource "aws_s3_bucket" "spa-app-bkt" {
   bucket = var.spa-app-bkt
   acl    = "private"  
   force_destroy = var.s3_force_destroy
+  versioning {
+    enabled = true
+  }
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
@@ -16,6 +19,9 @@ resource "aws_s3_bucket" "static-data-bkt" {
   bucket = var.static-data-bkt
   acl    = "private"  
   force_destroy = var.s3_force_destroy
+  versioning {
+    enabled = true
+  }
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
